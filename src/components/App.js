638e699bb546ec87482843f9c603a7634d7ba58b
Natalia <atvnataliajn@gmail.com>
2022-01-18 08:57:43 +0100

    add basic structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,43 @@
-import '../styles/App.scss';
-import { useState } from 'react';
+import "../styles/App.scss";
+import { useState } from "react";
+import studentsData from "../data/studentsData.json";
 
 function App() {
+  // States
+  const [students, setStudents] = useState(studentsData);
+
+
+  // Functions
+  const renderStudents= () => {
+    return students
+  };
+
   return (
     <div>
-      <h1>Hola mundo</h1> 
+      <h1>Adalabers- promo patatita</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Tutores</th>
+            <th>Especialidad</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Maricarmen</td>
+            <td>Amparo</td>
+            <td>Escandia</td>
+          </tr>
+
+          <tr>
+            <td>Columa 1 de la fila 2</td>
+            <td>Columa 2 de la fila 2</td>
+            <td>Columa 3 de la fila 2</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
